34136c7a51dd7fca09dbfe4df8326d6f274ed8c9
Lex <fischer84@mail.ru>
2022-06-06 09:09:19 +0300

    added middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
+
+const stringMiddleware = () => (next) => (action) => {
+        if (typeof action === 'string') {
+                return next({
+                        type: action
+                });
+        }
+        return next(action);
+}
 
 const enhancer = (createStore) => (...args) => {
         const store = createStore(...args);
 
         const oldDispatch = store.dispatch;
         store.dispatch = (action) => {
-                if (typeof actions === 'string') {
+                if (typeof action === 'string') {
                         return oldDispatch({
                                 type: action
                         });
@@ -23,8 +32,14 @@ const enhancer = (createStore) => (...args) => {
 const store = createStore(
                 combineReducers({heroes, filters}), 
                 compose(
-                        enhancer,
+                        applyMiddleware(stringMiddleware),
                         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                ));
+                )
+                
+                // compose(
+                //         enhancer,
+                //         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                // )
+);
 
 export default store;
